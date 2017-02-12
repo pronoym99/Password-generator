6f64d747a094e1d50d81d1f45e8fabd3818d4864
Pronoy Mandal <lukex9442@gmail.com>
2017-02-12 18:07:47 +0530

    Update otp genrator.py

--- otp genrator.py
+++ otp genrator.py
@@ -11,17 +11,17 @@ L4=["!","@","#","$","%","^","&","*"]                                            
 L=L1+L2+L3+L4                                                                                                       #megalist
 while True:
     try:
-        n=int(raw_input("How many digit password do you want to generate?:"))
+        n=int(raw_input("How many digit password do you want to generate?:"))                                       #Asking the user for required length
         if n==0:
             print"No null value passwords allowed"
         elif n!=0:
             break
     except ValueError:
-            print "please enter a proper integer indicating the number of digits"    
+            print "please enter a proper integer indicating the number of digits"                                 #Handling the error where an user enters a string value for the length of the otp (purposefully or otherwise)
 otp=""
 while len(otp)<n:
         i=random.randint(0,len(L)-1)
-        if type(L[i])!=str:
+        if type(L[i])!=str:                                                                                       #Handling the case of addition of integers to a string or vice versa
             otp+=str(L[i])
         else:
             otp+=L[i]
