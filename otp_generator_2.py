2c6d3ad5756a4765ae8837c2dc907964167080f8
Pronoy Mandal <lukex9442@gmail.com>
2020-02-27 01:12:25 +0530

    Add working logic from otp_generator.py

--- otp_generator_2.py
+++ otp_generator_2.py
@@ -6,7 +6,7 @@ import click
 from six import print_
 
 # functions from random for password generation
-from random import (choice,shuffle)
+from random import (choice, shuffle)
 # decorative ascii text
 from pyfiglet import figlet_format
 # your CLI app essentials
@@ -112,46 +112,7 @@ def askPasswordInformation():
     ]
 
     answers = prompt(questions, style=style)
-    pprint(answers)
-
-
-
-# questions = [
-#         {
-#             'type': 'input',
-#             'name': 'password length',
-#             'message': 'How many digit password do you want to generate?:',
-#             'validate': PasswordLengthValidator
-#         },
-#         {
-#             'type': 'checkbox',
-#             'qmark': '\U0001F600',
-#             'message': 'Select options ',
-#             'name': 'password options',
-#             'choices': [
-#                 Separator('=== Menu ==='),
-#                 {
-#                     'name': 'Numbers'
-#                 },
-#                 {
-#                     'name': 'Lowercase Alphabets'
-#                 },
-#                 {
-#                     'name': 'Uppercase Alphabets'
-#                 },
-#                 {
-#                     'name': 'Special characters'
-#
-#                 }
-#             ]
-#             # 'validate': PasswordOptionsValidator
-#         }
-#     ]
-#
-# answers = prompt(questions, style=style)
-# pprint(answers)
-
-
+    return answers
 
 
 @click.command()
@@ -163,12 +124,44 @@ def main():
     displayInfo("Welcome to Password generator CLI", "green")
 
     # a dictionary to store all ascii characters as strings
-    password_map = {1: '0123456789', 2: 'abcdefghijklmnopqrstuvwxyz',
-                    3: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 4: '!@#$%^&*()-_=+<>?/|\{}[]~'}
+    password_map = {'Numbers': '0123456789', 'Lowercase Alphabets': 'abcdefghijklmnopqrstuvwxyz',
+                    'Uppercase Alphabets': 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'Special characters': '!@#$%^&*()-_=+<>?/|\{}[]~'}
 
     main_use_string = ''  # megastring
 
-    askPasswordInformation()
+    # retrieve data from PyInquirer prompt
+    answers = askPasswordInformation()
+    # first element of 'answers' is the password length
+    user_choice_password_length = int(answers['password length'])
+    # while second element of 'answers' is a list of user choices
+    user_choices = answers['password options']
+
+    # initialise empty otp string
+    otp = ''
+
+    for choice_string in user_choices:
+        # enumerate over each of the strings in user_choice
+        current_string = password_map[choice_string]
+        # construct your megastring at the same time
+        main_use_string += current_string
+        # add atleast one character from each of the strings in user_choice
+        otp += choice(current_string)
+
+    # reduced target length
+    target_length_of_otp = user_choice_password_length - len(user_choices)
+
+    for _ in range(target_length_of_otp):
+        otp += choice(main_use_string)
+
+    # shuffle your otp once
+    # remember to convert to list type before shuffling and back to str type after shuffling
+    otp_list = list(otp)
+    shuffle(otp_list)
+    otp = ''.join(otp_list)
+
+    # display the final otp
+    print('Your one time password is-')
+    displayInfo(otp, "red")
 
 
 if __name__ == '__main__':
